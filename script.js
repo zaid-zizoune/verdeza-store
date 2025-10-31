71b29971c8abf467170ade840e7f70b54c43118d
Zaid zizoune <zaidzizoune@gmail.com>
2025-10-31 21:35:22 +0100

    Complete Home Page

--- script.js
+++ script.js
@@ -7,3 +7,25 @@
     currentIndex = (currentIndex + 1) % slides.length;
     slides[currentIndex].classList.add("active");
   }, 3000); 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-link");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  const scrollPos = window.scrollY + window.innerHeight / 2; // نقطة منتصف الشاشة
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (scrollPos >= sectionTop) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === "#" + current) {
+      link.classList.add("active");
+    }
+  });
+});
